8115962588b1db70061f63c62fbd25476ee5672c
eduhdev12 <eduhdev12@users.noreply.github.com>
2023-02-15 13:42:42 +0200

    feat(user): styles

--- src/auth/User.tsx
+++ src/auth/User.tsx
@@ -1,4 +1,5 @@
 import useSessionStore from "../store/sessionStore";
+import "../style/User.css";
 
 const User = () => {
   const session = useSessionStore();
@@ -6,11 +7,11 @@ const User = () => {
   if (session.data && session.data.id) {
     return (
       <div className="user-details">
-        <p>
+        <p className="zero-margin">
           Welcome, {session.data.username} ({session.data.email}) (
           {session.data.id})
         </p>
-        <p>Role: {session.data.role}</p>
+        <p className="zero-margin">Role: {session.data.role}</p>
         <button onClick={() => session.logOut()}>Log out</button>
       </div>
     );
