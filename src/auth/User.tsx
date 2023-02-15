c6a25a5dafeb624183c1715662b1662292802f3c
eduhdev12 <eduhdev12@users.noreply.github.com>
2023-02-15 13:33:00 +0200

    feat(web): added more user details

--- src/auth/User.tsx
+++ src/auth/User.tsx
@@ -6,7 +6,11 @@ const User = () => {
   if (session.data && session.data.id) {
     return (
       <div className="user-details">
-        <p>Welcome, {session.data.username}</p>
+        <p>
+          Welcome, {session.data.username} ({session.data.email}) (
+          {session.data.id})
+        </p>
+        <p>Role: {session.data.role}</p>
         <button onClick={() => session.logOut()}>Log out</button>
       </div>
     );
